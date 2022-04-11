1cc1f389f0d7c1ebd066337ccc4055a6d966d9ce
Tore Bjolseth <tbjolset@cisco.com>
2022-04-11 11:15:20 +0200

    filter to show free rooms only

--- html/roomstatus.js
+++ html/roomstatus.js
@@ -6,6 +6,9 @@ const validPattern = 'lys01-'; // TODO move to config
 
 const dataModel = {
   rooms: [],
+  filter: {
+    freeOnly: false,
+  },
 
   async fetchData() {
     const data = await fetch('/api/data');
@@ -14,6 +17,10 @@ const dataModel = {
     this.rooms = rooms
     .filter(r => r.deviceName.toLowerCase().includes(validPattern))
     .sort((r1, r2) => r1.deviceName.toLowerCase() < r2.deviceName.toLowerCase() ? -1 : 1);
+  },
+
+  getRooms() {
+    return this.filter.freeOnly ? this.rooms.filter(r => r.occupants < 1) : this.rooms;
   },
 
   getStatus(room) {
