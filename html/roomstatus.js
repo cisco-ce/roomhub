a4e7f43542e5ba8322320bdaf4e473ee329c1f63
Tore Bjolseth <tbjolset@cisco.com>
2022-04-11 12:17:18 +0200

    indicate people count and call status clearly on rooms

--- html/roomstatus.js
+++ html/roomstatus.js
@@ -1,6 +1,6 @@
 const $ = s => document.querySelector(s);
 const $$ = s => Array.from(document.querySelectorAll(s));
-const pollInterval = 60E3;
+const pollInterval = 10E3;
 
 const validPattern = 'lys01-'; // TODO move to config
 
@@ -13,7 +13,7 @@ const dataModel = {
   async fetchData() {
     const data = await fetch('/api/data');
     const rooms = await data.json();
-    // console.log('got', rooms);
+    console.log('got', rooms);
     this.rooms = rooms
     .filter(r => r.deviceName.toLowerCase().includes(validPattern))
     .sort((r1, r2) => r1.deviceName.toLowerCase() < r2.deviceName.toLowerCase() ? -1 : 1);
