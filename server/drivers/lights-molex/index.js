e0473ac3879031d5b6a2b24560e6aa620926e41a
German Cheung <102512136+German-Cheung-GC@users.noreply.github.com>
2024-12-12 21:25:15 +0800

    Support Multi-Zone ID

--- server/drivers/lights-molex/index.js
+++ server/drivers/lights-molex/index.js
@@ -26,17 +26,23 @@ function setMolexLight(gateway, zone, level, device) {
 async function onCommand(command, answer) {
   const config = Config.current();
   const device = config.devices?.find(d => d.device === command.device);
-
-  // console.log('on light command', command);
-  const { zone, gateway } = device.lights;
-  const level = parseInt(command.level);
-
-  try {
-    await setMolexLight(gateway, zone, level, command.device);
-    answer({ result: true });
+  
+  if (device && device.lights) {
+    try {
+      const { zone, gateway } = device.lights;
+      const zoneList = zone.includes(',') ? zone.split(',') : [zone];
+      const level = parseInt(command.level);
+      for (const z of zoneList) {
+        await setMolexLight(gateway, z.trim(), level, command.device);
+      }
+      answer({ result: true });
+    }
+    catch(e) {
+      answer({ error: 'error' });
+    }
   }
-  catch(e) {
-    answer({ error: 'error' });
+  else {
+    answer('Device does not have light configured', 400);
   }
 }
 
